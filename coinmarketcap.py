42322f53fe454a7c783edd35663c28dea97c55ab
phonen <pioul@tom.com>
2018-03-04 02:05:20 +0800

    1

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-from requests import Session
+import requests
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
@@ -12,7 +12,7 @@ sys.path.append(root + '/python')
 
 api = 'https://api.coinmarketcap.com/v1/ticker/'
 
-request = Session()
-response = request.get(api)
+
+response = requests.get(api)
 print(response)
 
