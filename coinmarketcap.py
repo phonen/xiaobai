97987bdd7c2144a93699648f46b0b80985399b2d
phonen <pioul@tom.com>
2018-03-04 01:48:49 +0800

    1

---
+++ coinmarketcap.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root + '/python')
+
+import ccxt  # noqa: E402
+import key
+
+coinmarket = ccxt.coinmarket()
+print(coinmarket.request())
