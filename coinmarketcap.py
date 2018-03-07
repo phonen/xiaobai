1a3b7e91a3b956d42fca67eafe915f0df579c04f
phonen <pioul@tom.com>
2018-03-07 11:27:28 +0800

    1

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import json
-from flask import Flask,request
+import pymysql
 from requests import Session
 from requests.utils import default_user_agent
 from requests.exceptions import HTTPError, Timeout, TooManyRedirects, RequestException
@@ -12,7 +12,6 @@ from ssl import SSLError
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
 
-app=Flask(__name__)
 
 
 def handle_rest_response(response):
@@ -23,42 +22,39 @@ def handle_rest_response(response):
     else:
         return response
 
+api = 'https://api.coinmarketcap.com/v1/ticker/?limit=0'
+session = None  # Session () by default
+logger = None  # logging.getLogger(__name__) by default
+userAgent = None
+userAgents = {
+    'chrome': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+    'chrome39': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+}
+body = None
+if body:
+    body = body.encode()
+last_http_response = None
+last_json_response = None
+last_response_headers = None
+userAgent = default_user_agent()
+timeout = 10000  # milliseconds = seconds * 1000
+session = session if session else Session()
+response = session.request('GET', api, data=body, headers=None, timeout=int(timeout / 1000))
+last_http_response = response.text
+result = handle_rest_response(last_http_response)
+db = pymysql.connect("localhost","root","KeYpZrZx","btc")
+cursor = db.cursor()
+
+for data in result:
+    sql = """INSERT INTO coinmarket(symbol,price,volume,ctime)
+              values (data['symbol'],data['price'],data['24h_volume_usd'],now())"""
+
+    try:
+        cursor.execute(sql)
+        db.comit()
+    except:
+        db.rollback()
+db.close()
 
 
-@app.route('/')
-def hello_world():
-    return "Hello World~~~"
-
-
-@app.route('/cap')
-def cap():
-    api = 'https://api.coinmarketcap.com/v1/ticker/'
-
-    session = None  # Session () by default
-    logger = None  # logging.getLogger(__name__) by default
-    userAgent = None
-    userAgents = {
-        'chrome': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
-        'chrome39': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
-    }
-    body = None
-    if body:
-        body = body.encode()
-    last_http_response = None
-    last_json_response = None
-    last_response_headers = None
-    userAgent = default_user_agent()
-    timeout = 10000  # milliseconds = seconds * 1000
-    session = session if session else Session()
-    response = session.request('GET', api, data=body, headers=None, timeout=int(timeout / 1000))
-    last_http_response = response.text
-    result = handle_rest_response(last_http_response)
-    table = '<table boder=1><tr><td>币</td><td>交易量$</td></tr>'
-    for data in result:
-        table = table + '<tr><td>' + data['symbol'] + '</td><td>' + data['24h_volume_usd'] + '</td></tr>'
-
-    table = table + '</table>'
-    return table
-if __name__ == '__main__':
-    app.run('0.0.0.0', 8899)
 
