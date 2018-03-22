8aabf7a5fa35e62132cfef06bbe9fb22f4e99cb0
phonen <pioul@tom.com>
2018-03-22 21:57:57 +0800

    1

--- coinmarket_12h.py
+++ coinmarket_12h.py
@@ -11,35 +11,37 @@ db = pymysql.connect("localhost","root","KeYpZrZx","btc")
 cursor = db.cursor()
 cursor.execute("truncate table coinmarket_12h")
 db.commit()
-sql = """select symbol from coinmarket_last"""
+
+sql1 = """select symbol,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-12*3600 and update_time> UNIX_TIMESTAMP() -12*3600-600"""
+cursor.execute(sql1)
+result1 = cursor.fetchall()
+coin = []
+for row in result1:
+    coin[row[0]]['price'] = row[1]
+    coin[row[0]]['volume'] = row[2]
+
+sql = """select symbol,price_usd,volume from coinmarket_last"""
 cursor.execute(sql)
 
 result = cursor.fetchall()
+
 for row in result:
     symbol = row[0]
-    sql1 = """SELECT price_usd,volume,last_updated from coinmarket where symbol='%s' ORDER BY last_updated desc limit 1""" % (symbol)
-    cursor.execute(sql1)
-    result1 = cursor.fetchone()
-    price1 = result1[0]
-    volume1 = result1[1]
-    last1 = result1[2]
-    sql2 = """select price_usd,volume,last_updated from coinmarket where symbol='%s' and last_updated<=%s - 12*3600 order by last_updated desc limit 1""" % (symbol,last1)
-    cursor.execute(sql2)
-    result2 = cursor.fetchone()
-    price2 = result2[0]
-    volume2 = result2[1]
-    last2 = result2[2]
-    print(sql1)
-    print(sql2)
+    price = row[1] - coin[symbol]['price']
+    volume = row[1] - coin[symbol]['volume']
     try:
         sql = """insert into coinmarket_12h(symbol,price,volume) values ('%s',%s,%s)""" % (
-        symbol, price1 - price2, volume1 - volume2)
+        symbol, price, volume)
         print(sql)
         cursor.execute(sql)
         db.commit()
     except:
         print('error')
         db.rollback()
+
+
+
+
 
 db.close()
 
