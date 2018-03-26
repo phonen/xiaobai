03801909f0b001b9235542886a3057c0139aefd2
phonen <pioul@tom.com>
2018-03-26 21:48:00 +0800

    1

--- coinmarket_48h.py
+++ coinmarket_48h.py
@@ -12,20 +12,9 @@ cursor = db.cursor()
 cursor.execute("truncate table coinmarket_48h")
 db.commit()
 
-sql1 = """select symbol,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-48*3600 and update_time> UNIX_TIMESTAMP() -48*3600-600"""
-cursor.execute(sql1)
-result1 = cursor.fetchall()
-coin = {}
-coind = {}
-for row in result1:
-    symbol = row[0]
-    coind['price'] = row[1]
-    coind['volume'] = row[2]
 
-    coin[symbol] = coind
-
-
-sql = """select symbol,price_usd,volume from coinmarket_last"""
+sql = """select a.symbol as symbol,a.price as price,a.volume as volume,b.price_usd as price12,b.volume as volume12,c.price_usd as price48,c.volume as volume48 from coinmarket_last a,(select symbol,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-12*3600 and update_time> UNIX_TIMESTAMP() -12*3600-600) b,(select symbol,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-48*3600 and update_time> UNIX_TIMESTAMP() -48*3600-600) c
+where a.symbol=b.symbol and a.symbol=c.symbol"""
 cursor.execute(sql)
 
 result = cursor.fetchall()
@@ -33,18 +22,21 @@ result = cursor.fetchall()
 for row in result:
 
     symbol = row[0]
-    if symbol in coin:
-        price = row[1] - coin[symbol]['price']
-        volume = row[2] - coin[symbol]['volume']
-        try:
-            sql = """insert into coinmarket_48h(symbol,price,volume) values ('%s',%s,%s)""" % (
-                symbol, price, volume)
-            print(sql)
-            cursor.execute(sql)
-            db.commit()
-        except:
-            print('error')
-            db.rollback()
+    price = row[1]
+    volume = row[2]
+    price12 = row[3]
+    volume12 = row[4]
+    price48 = row[5]
+    volume48 = row[6]
+    try:
+        sql = """insert into coinmarket_48h(symbol,price,volume，price12,volume12,price48,volume48) values ('%s',%s,%s,%s,%s,%s,%s)""" % (
+            symbol, price, volume,price12,volume12,price48,volume48)
+        print(sql)
+        cursor.execute(sql)
+        db.commit()
+    except:
+        print('error')
+        db.rollback()
 
 
 
