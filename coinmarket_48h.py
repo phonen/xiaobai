bc37635a8e73f196d17570342d9c63d9273a2f44
phonen <pioul@tom.com>
2018-03-30 09:34:27 +0800

    1

--- coinmarket_48h.py
+++ coinmarket_48h.py
@@ -13,24 +13,28 @@ cursor.execute("truncate table coinmarket_48h")
 db.commit()
 
 
-sql = """select a.symbol as symbol,a.price_usd as price,a.volume as volume,b.price_usd as price12,b.volume as volume12,c.price_usd as price48,c.volume as volume48 from coinmarket_last a,(select symbol,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-12*3600 and update_time> UNIX_TIMESTAMP() -12*3600-600) b,(select symbol,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-48*3600 and update_time> UNIX_TIMESTAMP() -48*3600-600) c
-where a.symbol=b.symbol and a.symbol=c.symbol"""
+sql = """select d.id as coinid,d.name as name,d.symbol as symbol,a.price_usd as price,a.volume as volume,b.price_usd as price12,b.volume as volume12,c.price_usd as price48,c.volume as volume48 
+from coins d,coinmarket_last a,
+(select coinid,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-12*3600 and update_time> UNIX_TIMESTAMP() -12*3600-600) b,
+(select coinid,price_usd,volume from coinmarket where update_time<=UNIX_TIMESTAMP()-48*3600 and update_time> UNIX_TIMESTAMP() -48*3600-600) c
+where d.id=a.coinid and d.id=b.coinid and d.id=c.coinid"""
 cursor.execute(sql)
 
 result = cursor.fetchall()
 
 for row in result:
-
-    symbol = row[0]
-    price = row[1]
-    volume = row[2]
-    price12 = row[3]
-    volume12 = row[4]
-    price48 = row[5]
-    volume48 = row[6]
+    id = row[0]
+    name = row[1]
+    symbol = row[2]
+    price = row[3]
+    volume = row[4]
+    price12 = row[5]
+    volume12 = row[6]
+    price48 = row[7]
+    volume48 = row[8]
     try:
-        sql = """insert into coinmarket_48h(symbol,price,volume,price12,volume12,price48,volume48) values ('%s',%s,%s,%s,%s,%s,%s)""" % (
-            symbol, price, volume,price12,volume12,price48,volume48)
+        sql = """insert into coinmarket_48h(id,symbol,price,volume,price12,volume12,price48,volume48) values ('%s','%s',%s,%s,%s,%s,%s,%s)""" % (
+            id,symbol, price, volume,price12,volume12,price48,volume48)
         print(sql)
         cursor.execute(sql)
         db.commit()
